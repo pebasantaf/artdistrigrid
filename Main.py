5f028113f051542f9eb1f34c9c63d616cb29f8a1
pebasantaf <pedrobasantafranco@outlook.com>
2020-06-22 15:48:05 +0200

    issues with model saving fixed

--- Main.py
+++ Main.py
@@ -11,7 +11,7 @@ from classes.EarlyStoppingByLossVal import EarlyStoppingByLossVal
 ''' PATHS '''
 train_path = r'C:\Users\Usuario\Documents\Universidad\TUM\Subjects\3rd semester\Research Internship\ANN_grids\Data02'
 test_path = r'C:\Users\Usuario\Documents\Universidad\TUM\Subjects\3rd semester\Research Internship\ANN_grids\Test'
-model_path = r'C:\Users\Usuario\Documents\Universidad\TUM\Subjects\3rd semester\Research Internship\ANN_grids\model'
+model_path = r'C:\Users\Usuario\Documents\Universidad\TUM\Subjects\3rd semester\Research Internship\ANN_grids\model\model{}.h5'
 
 ''' USER INPUT '''
 inout = [13, 13]
@@ -19,7 +19,7 @@ look_back = 1
 drop = ['All calculations']
 loss_functions = 'mean_squared_error'
 metrics = ['mse', 'mae']
-epochs = 15
+epochs = 3
 validation_split = 0.25
 
 ''' DATA IMPORT '''
@@ -36,8 +36,8 @@ l4 = model.add(layers.Dense(inout[1])) #fully connected layer. What i would unde
 
 opt = optimizers.RMSprop(lr=1e-03) #how fast the learning rate decays. this helps finding the miminum better
 
-callbacks = [EarlyStoppingByLossVal(monitor='val_loss', value=0.002),
-     ModelCheckpoint(filepath=model_path.format(int(time.time())), save_best_only=True)]
+callbacks = [EarlyStoppingByLossVal(monitor='val_loss', value=0.002)]
+     #ModelCheckpoint(filepath=model_path.format(int(time.time())), save_best_only=True)]
 
 #compiling the model. Defining some of sthe features for the fit like the type of loss function, the optimizer and metrics that are interesting for us
 
@@ -45,3 +45,9 @@ model.compile(loss=loss_functions,
              metrics=metrics)  # accuracy only valid for classification tasks
 
 history = model.fit(X, Y, epochs=epochs, validation_split=validation_split, callbacks=callbacks, batch_size=200)
+
+model.save(model_path.format(int(time.time())))
+
+fig1, fig2 = TestPlot(model, Tests)
+
+fig3 = evaluateTraining(history)
