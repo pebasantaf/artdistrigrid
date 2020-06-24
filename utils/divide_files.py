cb945ef18a917d42bd33b49205ba7cd7945e6c1b
pebasantaf <pedrobasantafranco@outlook.com>
2020-06-24 14:04:56 +0200

    delete all files

--- utils/divide_files.py
+++
@@ -1,8 +0,0 @@
-import glob
-import shutil
-path = r'C:\Users\Usuario\Documents\Universidad\TUM\Subjects\3rd semester\Research Internship\ANN_grids\Data02'
-dest = r'C:\Users\Usuario\Documents\Universidad\TUM\Subjects\3rd semester\Research Internship\ANN_grids\Test02'
-test_files = glob.glob(path + "/*.csv")# keeping directories in a list
-halftomove = test_files[1:-1:2]
-for f in halftomove:
-    shutil.move(f, dest)
